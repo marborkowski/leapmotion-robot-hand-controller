611dc64332c6478b70d2fb0b7717831af9f6361a
Marcin Borkowski <marborkowski@gmail.com>
2015-10-17 23:05:49 +0200

    Working app.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# leapmotion-robot-hand-controller
+# Robot hand controller using Node.js
+
+Description here...
+
+### Installation
+
+Download and install Leap Motion SDK from the official site: https://www.leapmotion.com/
+Then:
+
+```sh
+$ npm install
+```
+
+-- will install the all unnecessary npm modules.
+
+### Running
+```sh
+$ node app.js
+```
---
+++ app.js
@@ -0,0 +1,178 @@
+(function() {
+
+    /**
+     * Define '_self' variable
+     * as the reference to 'this'.
+     */
+    var _self = this;
+
+    /**
+     * Load necessary npm modules.
+     * @type {{cylon: Object, fs: Object, util: Object, serialport: Object}}
+     */
+    this.modules = {
+        cylon: require('cylon'),
+        fs: require('fs'),
+        util: require('util'),
+        serialport: require('serialport')
+    };
+
+    this.app = {
+
+        /**
+         * Various app states.
+         */
+        states: {
+            isPalmOpen: true
+        },
+
+        /**
+         * Init Cylon.js module
+         * and define the all needed
+         * devices and connections.
+         */
+        cylon: _self.modules.cylon.robot({
+            connections: {
+                leapmotion: {
+                    adaptor: 'leapmotion'
+                }
+            },
+            devices: {
+                leapmotion: {
+                    driver: 'leapmotion'
+                }
+            },
+            work: function(my) {
+                /**
+                 * Handling 'hand' event
+                 * from Leap Motion controller.
+                 */
+                my.leapmotion.on('hand', function(payload) {
+
+                    /**
+                     * Calculate straight fingers.
+                     * @type {number}
+                     */
+                    var straightFingers = 0;
+
+                    /**
+                     * Iterate the all fingers.
+                     */
+                    payload.fingers.forEach(function(finger, index) {
+                        /**
+                         * If finger is straight,
+                         * we should increase the value
+                         * of 'straightFinters' with 1.
+                         */
+                        if(finger.extended) {
+                            straightFingers++;
+                        }
+                    });
+
+                    /**
+                     * Where the 'straightFingers' number
+                     * is equal to or greater than 4,
+                     * we should presume that the palm is open.
+                     * @type {boolean}
+                     */
+                    var isPalmOpen = straightFingers >= 4;
+
+                    /**
+                     * If the current state is different
+                     * than previous one, update it
+                     * and send the necessary data using serial port.
+                     */
+                    if(_self.app.states.isPalmOpen !== isPalmOpen) {
+                        _self.app.states.isPalmOpen = isPalmOpen;
+                        _self.app.serial.router.write(isPalmOpen.toString());
+                        console.log('Palm is %s', isPalmOpen ? 'open' : 'closed');
+                    }
+                });
+            }
+        }),
+        serial: {
+          router: null,
+          start: function(port) {
+              /**
+               * Init the serial communication
+               * on port described with 'port' attribute.
+               * @type {*|SerialPort}
+               */
+              _self.app.serial.router = new _self.modules.serialport.SerialPort(port, {
+                  baudrate: 115200
+              }, true);
+
+              /**
+               * Monitor serial port states.
+               */
+              _self.app.serial.router.on('open', function() {
+                  console.log('SerialPort opened on %s', port);
+
+                  /**
+                   * Start Cylon library
+                   * if the serial communication is established.
+                   */
+                  _self.app.cylon.start();
+              });
+
+              _self.app.serial.router.on('close', function() {
+                  console.log('SerialPort closed.');
+              });
+          }
+        },
+        start: function() {
+
+            /**
+             * List the all available USB devices
+             * and find the Arduino compatible board.
+             */
+            _self.modules.serialport.list(function (err, ports) {
+
+                /**
+                 * Filter Arduino compatible devices.
+                 */
+                var arduino = ports.filter(function(port) {
+                    return /^arduino/i.test(port.manufacturer);
+                });
+
+                /**
+                 * Behavior depends on the number of compatible devices.
+                 */
+                switch(arduino.length) {
+
+                    // No Arduino compatible device...
+                    case 0:
+                        console.error('Plug in your Arduino board first!');
+                        process.exit();
+                        break;
+
+                    // We found the one and only Arduino device...
+                    case 1:
+                        var board = arduino.pop();
+                        console.log('Connecting to Arduino board %s on port %s', board.manufacturer, board.comName);
+                        _self.app.serial.start(board.comName);
+                        break;
+
+                    // Otherwise...
+                    default:
+                        console.error('There is more than one Arduino board plugged into your computer. Please reduce it to ONE!');
+                        process.exit();
+                }
+
+            });
+
+
+        }
+    };
+
+    this.app.start();
+
+    /**
+     * Kill app on CTRL+C
+     */
+    process.on('SIGINT', function() {
+        console.log('Bye!');
+        _self.app.serial.router.close();
+        process.exit();
+    });
+})();
